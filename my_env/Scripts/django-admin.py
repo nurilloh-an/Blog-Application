1b87d46c67a364192a028fd7fd2005f1f10d7faa
nurilloh.2004 <nurillohpython@gmail.com>
2022-07-19 22:09:48 +0500

    all need files created

---
+++ my_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nuril\OneDrive\Desktop\SELFSTUDY\Blog-Application\my_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
