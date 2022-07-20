2df57dacc4a04c34a98c5b67926cf350284450b3
nurilloh.2004 <nurillohpython@gmail.com>
2022-07-20 10:14:26 +0500

    models

--- config/blog/admin.py
+++ config/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'author', 'publish', 'status')
+
+
+
+
+# >>>Post.objects.filter(publish__year=2020) \
+# >>> .filter(author__username='admin'
--- config/blog/models.py
+++ config/blog/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                    self).get_queryset()\
+                    .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -18,7 +30,20 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                                 choices=STATUS_CHOICES,
                                 default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day, self.slug])
+
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+
