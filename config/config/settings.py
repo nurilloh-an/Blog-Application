3dc6c9c52789744563f861cd52c2169bca14f35e
nurilloh.2004 <nurillohpython@gmail.com>
2022-07-21 11:16:33 +0500

    Finished for now

--- config/config/settings.py
+++ config/config/settings.py
@@ -79,7 +79,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alharamin1004@gmail.com'
-EMAIL_HOST_PASSWORD = '*******'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
