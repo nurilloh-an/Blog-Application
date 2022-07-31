7f02a3467e0fb587803474944f617d796be9177c
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2022-07-31 13:40:07 +0500

    all

--- config/blog/views.py
+++ config/blog/views.py
@@ -15,7 +15,7 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
 
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    paginator = Paginator(object_list, 2) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
